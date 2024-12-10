e7af83ea94d9a9ba9043dc0c1d8f513a55f66565
Fred <huertaserick@yahoo.ie>
2024-12-10 17:14:36 +0000

    Tailwind and React day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./index.css";
+import RecipeCard from "./components/RecipeCard/RecipeCard";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="w-full border-2 border-red-300 h-full"></div>
+      <RecipeCard />
     </>
   );
 }
---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,32 @@
+const RecipeCard = () => {
+  return (
+    <div className="border-2 max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
+      <img className="w-full h-48" src="" alt="Image here" />
+
+      <div className="px-6 py-4">
+        <h2 className="font-bold text-xl mb-2 truncate">Recipe Here</h2>
+        <p className="text-gray-700 text-sm line-clamp-3">Description here</p>
+      </div>
+
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-blue-100 text-blue-800 text-sm px-3 py-1 rounded-full">
+          Tag 1
+        </span>
+        <span className="inline-block bg-green-100 text-green-800 text-sm px-3 py-1 rounded-full">
+          Tag 2
+        </span>
+        <span className="inline-block bg-yellow-100 text-yellow-800 text-sm px-3 py-1 rounded-full">
+          Tag 3
+        </span>
+      </div>
+
+      <div className="px-6 py-4 flex justify-between items-center">
+        <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold">
+          View Recipe
+        </button>
+        <span className="text-gray-600 text-sm"> ⭐⭐⭐⭐</span>
+      </div>
+    </div>
+  );
+};
+export default RecipeCard;
