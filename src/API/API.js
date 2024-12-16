d6a15a559177bad9154a6e639a8d008c64dc8373
Fred <huertaserick@yahoo.ie>
2024-12-16 11:35:41 +0000

    tailwind x react

--- src/API/API.js
+++ src/API/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.FOOD_API_KEY;
+const apiKey = import.meta.env.VITE_FOOD_API_KEY;
 
 const apiClient = axios.create({
   baseURL: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 
 export const fetchRecipe = async (tags = "", number = 1) => {
   try {
-    const response = await apiClient.get("/recipe/random", {
+    const response = await apiClient.get("/recipes/random", {
       params: { tags, number },
     });
     return response.data;
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,16 +1,43 @@
+import { useEffect, useState } from "react";
+import { fetchRecipe } from "../../API/API";
+import DOMPurify from "dompurify";
+
 const RecipeCard = () => {
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const pickedRecipe = async () => {
+      try {
+        const data = await fetchRecipe("vegetarian,dessert");
+        setRecipe(data.recipes[0]);
+        console.log(data.recipes[0]);
+      } catch (error) {
+        setError(error.message || "Something went wrong..");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    pickedRecipe();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const cleanedSummary = DOMPurify.sanitize(recipe.summary);
+
   return (
     <div className="border-2 max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
-      <img className="w-full h-48" src="" alt="Image here" />
+      <img className="w-full h-48" src={recipe.image} alt="Image here" />
 
       <div className="px-6 py-4">
-        <h2 className="font-bold text-xl mb-2 truncate">Recipe Here</h2>
-        <p className="text-gray-700 text-sm line-clamp-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsa
-          alias a eaque vitae qui autem saepe aut cupiditate amet consectetur
-          modi labore quisquam provident voluptatem aliquid officiis, repellat
-          animi!
-        </p>
+        <h2 className="font-bold text-xl mb-2 truncate">{recipe.title}</h2>
+        <p
+          className="text-gray-700 text-sm line-clamp-5"
+          dangerouslySetInnerHTML={{ __html: cleanedSummary }}
+        />
       </div>
 
       <div className="px-6 pt-4 pb-2">
@@ -26,9 +53,14 @@ const RecipeCard = () => {
       </div>
 
       <div className="px-6 py-4 flex justify-between items-center">
-        <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold">
+        <a
+          className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold"
+          href={recipe.spoonacularSourceUrl}
+          target="_blank"
+        >
           View Recipe
-        </button>
+        </a>
+
         <span className="text-gray-600 text-sm"> ⭐⭐⭐⭐</span>
       </div>
     </div>
